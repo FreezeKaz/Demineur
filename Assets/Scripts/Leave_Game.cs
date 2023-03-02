9b2010fb780f4dac62089761ee32dd436575abb6
FreezeKaz <freezekaz.dement@gmail.com>
2023-03-02 21:46:51 +0100

    Final Game 1.1

---
+++ Assets/Scripts/Leave_Game.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leave_Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+   
+}
